fa586dda2ce7d82e7a1209284ecf03ee3175618b
Lucrecia Galarza <44737561@ROQUEGONZALEZ.ORG>
2024-09-24 11:54:02 -0300

    Agregados modelo, controlador, migración y seeders para Estudiantes

--- roque/routes/web.php
+++ roque/routes/web.php
@@ -2,17 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\ProductoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+route::get('/estudiantes', [EstudianteController::class, 'index'])->name('estudiantes.index');Route::resource('productos', ProductoController::class);
+Route::get('/productos', [EstudianteController::class, 'index']);
+
+
 
 Route::get('/', function () {
     return view('welcome');
